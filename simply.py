936d445bf88371913ca3b7fff690e4e4421382dc
Grzegorzp1979 <pociechagrzegorz@gmail.com>
2022-12-29 18:26:48 +0100

    next commit

--- simply.py
+++ simply.py
@@ -1,4 +1,30 @@
 """
 Sorting Method
 """
-fruits = ["orange", "watermelon", "apple", "kiwi", "pear", "cherry"]
+
+def find_smallest(arr):
+    """
+    find smallest index
+    """
+    smallest = arr[0]
+    smallest_index = 0
+    for i in range(1, len(arr)):
+        if arr[i] < smallest:
+            smallest = arr[i]
+            smallest_index = i
+    return smallest_index
+
+def selection_sort(arr):
+    """
+    sorting function
+    """
+    new_arr = []
+    for i in range(len(arr)):
+        smallest = find_smallest(arr)
+        new_arr.append(arr.pop(smallest))
+    return new_arr
+
+# fruits = ["orange", "watermelon", "apple", "kiwi", "pear", "cherry"]
+numbers = [77, 90, 3, 5, 66, 1, 786, 55, 11]
+# print(find_smallest(numbers))
+print(selection_sort(numbers))
